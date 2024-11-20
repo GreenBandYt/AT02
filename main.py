a8c64b9996eb24ba33e5e92e9293ba34236a0fcb
Yuriy Bandura <bandurayv@gmail.com>
2024-11-21 00:24:18 +0300

    Add_Part_2

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-import pytest
-
-def test_add2():
-    assert 1+1 == 2
-
-def test_sub2():
-    assert 3-2 == 1
+def check(number):
+    return number % 2 == 0
--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-import unittest
+import pytest
+from main import check
+
+def test_check():
+    assert check(6) == True
+
+def test_check2():
+    assert check(3) == False
+
+@pytest.mark.parametrize("number, expected", [
+    (2, True),
+    (5, False),
+    (0, True),
+    (56, True),
+    (-3, False)
+])
+
+def test_check_with_params(number, expected):
+    assert check(number) == expected
 
 
-class TestMath(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(1+1, 2)
 
-    def test_subtract(self):
-        self.assertEqual(3-2, 1)
-
-if __name__ == '__main__':
-    unittest.main()
